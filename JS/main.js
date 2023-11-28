7f9ff2b8460034fd839251d43d38e5589fd53a62
weru <weru.marsha@gmail.com>
2023-11-27 23:45:27 -0800

    Change body color

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
 //Functionality for increasing number gradually from 0 to 60%
 let number = document.getElementById("number");
 let counter = 0;
+setInterval(() => {
+    if(counter == 60){
+        clearInterval();
+    }else{
+         counter += 1;
+    number.innerHTML = counter + "%";
+    }
+    
+}, 30);
